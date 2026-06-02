a1ff75ea3b131b6b4dfc743a33c9894d8af15c6c
subbajandyala <subba.jandyala@gmail.com>
2026-06-02 16:47:34 +0530

    Update SENSIBULL_COOKIE and User-Agent headers

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const app = express();
 
 app.use(cors());
 
-const SENSIBULL_COOKIE = `sb_rudder_utm=%22utm_campaign%22%3A%22option-chain-invoked%22%2C%22utm_medium%22%3A%22widget%22%2C%22utm_source%22%3A%22Kite3Web%22; _ga=GA1.1.1212815647.1773819787; rl_page_init_referrer=RudderEncrypt%3AU2FsdGVkX18PVoaufhd1dhHPRHqcnfS0%2BA51kGXOXUdh3qZRP M4CeXF%2BLsfmNrQb; rl_page_init_referring_domain=RudderEncrypt%3AU2FsdGVkX180BJRQOl1yRr%2FqVmx88yzpRGI1snasnxa3X11tRddbNUB5UkTdEdDz; _gcl_au=1.1.1485466765.1780301843; _ga_NC7XJTRTDX=GS2.1.s1780388777$o3$g1$t1780397875$j60$l0$h0; rl_anonymous_id=RudderEncrypt%3AU2FsdGVkX19%2FNGMXMuUSFmLnVadNsmA5K0BY4Bz0BpMpEapn1AGztzc%2BV4zmC5sa3Q1FPUxt10iq9B0G3us94w%3D%3D; rl_user_id=RudderEncrypt%3AU2FsdGVkX19Wq9WuiHrwUQz8hkLxVWSzEafHOkWgIbPpKCUccFtzJrKulatcGvpLg6AB4RnMmb1TLyCUYH5sNg%3D%3D; rl_trait=RudderEncrypt%3AU2FsdGVkX19coeC7XeZQERgV1JS5V9U9h%2BjDV98In2MjUKKR30tgw5npk3CQWq%2Fku6phI8%2Bygd20nvv1w1AYUMyn0Q0xSJ1%2FHpTUZ3xc%2B6I0wTgKxy%2F%2BIGKBi0%2BFJGpgCLHUm6B3s2Lg6eN6pz9FToGIsALYt9rsu2%2FDO4jYdZ8if%2BwDgg5tfc1PPuL%2BCBA4pKYX09%Xmmnix3%2B0MmYhFCwTt1G%2FYjc3mVBMzP%2Ben1yCfAjwJt0CPxzQ079Vi5BSBjfodUH%2BgTHfNk1X5py4xsXotUqikhRtk%2Bd7J7FmSeltk%2F3dmYehDMGyfurjYscrSk36nydtqnVZG5VigMAiqur%2BpzjA%2BgqXeoRSANbNy6XE5B5HtNzqHGNJwvo%2FT0rTTJDqG%2B2g2DvwhEM6v3Ltrh4eThl5gTYIWtSVPNDh7vnqFmyDqPwuNfPUz9pOkZXYam51MxkCnQifcGXtmiG2wXLnrla756QuEyfFnF8eVVRbczL6OFoXpHZFXuirBNP3KFjNfJHAJM3czGV%2F%2FyfA%3D%3D; rl_session=RudderEncrypt%3AU2FsdGVkX19%2FOPmPAGnkzaX58gMyUvTTUu3HLWmgQSv%2FDpX6SWzI26eSBIirQ%2BF%2Bv2vL5C4BxnbVSnCKJ1%2F7edT8JXsjpb2VEe%2Bptaq4zRBcSHZ3hHh%2FsMcuzP2YEuOengs1RPCowGLRNLBxDa3fbI7Q%3D%3D`;
+const SENSIBULL_COOKIE = `sb_rudder_utm=%22utm_campaign%22%3A%22option-chain-invoked%22%2C%22utm_medium%22%3A%22widget%22%2C%22utm_source%22%3A%22Kite3Web%22; _ga=GA1.1.1212815647.1773819787; rl_page_init_referrer=RudderEncrypt%3AU2FsdGVkX18PVoaufhd1dhHPRHqcnfS0%2BA5lkGXOXUdh3qZRPM4CeXF%2BLsfmNrQb; rl_page_init_referring_domain=RudderEncrypt%3AU2FsdGVkX180BJRQOllyRr%2FqVmx88yzpRGIlsnasnxa3X11tRddbNUB5UkTdEdDz; _gcl_au=1.1.1485466765.1780301843; bkd_ref=5Xvy4ii3LrEoq5r; access_token=0ASVbUw10sT_f0PDo8XEpRJ7_BtUe4dDN-GicUykBzY; _ga_NC7XJTRTDX=GS2.1.s1780388777$o3$g1$t1780398702$j59$l0$h0; _cfuvid=L2nr8KpkJji7fdzeTThlavNiTDT7xZAOZUmO7rgoJ5k-1780398703.194655-1.0.1.1-DUYIZbaBeH02YqKszRRT30WeTHlsg6pCdODX4wuLxfc; rl_anonymous_id=RudderEncrypt%3AU2FsdGVkX1%2BE6HZqW0mCpnjJASl2aTuZTSDGGgG3rNPyazLcVzprdlDr3w3hsUdUkIGu3JSMm4ySgklzrKfOKQ%3D%3D; rl_user_id=RudderEncrypt%3AU2FsdGVkX1%2FENJuwa3sgKudqt1pbxCAWkn%2BRaUXfW9OvNQjx8syzcF8LeSsNWEnltnfEUzn1WXu9r9tfbg32OQ%3D%3D; rl_trait=RudderEncrypt%3AU2FsdGVkX19jRXJwTVNxG8AYso0ElVf%2BHt9FzkLWWTEbCr6AbyXdBc8Oml%2FL%2BUal8%2FzC1%2Bh1MyP00AinytSbRDD1w94mTZoi97cl8QoRQpFJcRWMN7EFTbrY7%2F4PcDyO7pNdKIZJae2FbOH17O91%2Bijca5DCGNI%2FrOVQ0uT9ERQdYHnglImBoZTk8g2hDeJ9ieakYAb5%2Fk%2BXV5McvuS7wa8hl0CX5L1pEFMSKolWDRkxlrC%2FB2bR1pPl7xjzFCEVmlF0Wfpm3UCOiUElnhbiclp0hmVvwM7bNU7vObHzT6L0%2BFYKmhgLrq8zyeRc0MuzN5yqrJWwbaScCpMtlIm%2BBm3ZlowrEID6f2xpxdcn5ZvFX2SdV7mkCaKV0SjhPdQu%2Bjt7DT7T9vzxFdS0yPg8G6Q5nHqBxX6mOx71dC2AEhD%2BjqX7m%2FQROAHDm8Qdp7uOhKEGooz1Kw%2BXDvxBHt1UNmGXHpX1mNYip2lZFHCQpqFM5eUKrGBdSie3GBWAXR%2FkAvygUXp6M6tQQo%2Bz4NhPcg%3D%3D; rl_session=RudderEncrypt%3AU2FsdGVkX19F2SZpiVrO7uzJGgZXPQ0NA104QDyJ92%2BqSAD1EYMhirg1hWx%2BmOt7hLit3Tf6x3QDFFCJXNhznPviEdpm4mA8PP2TE09zd48aOS376Bb%2FcZoLiMrqVXAt6V9T9B1D3xp5FYjuDEqK9g%3D%3D`;
 
 const HEADERS = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36',
   'Accept': 'application/json, text/plain, */*',
   'Accept-Language': 'en-US,en;q=0.9',
   'Referer': 'https://web.sensibull.com/',
   'Origin': 'https://web.sensibull.com',
+  'sec-ch-ua': '"Chromium";v="148", "Google Chrome";v="148", "Not/A)Brand";v="99"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"Windows"',
   'cookie': SENSIBULL_COOKIE
 };
 
@@ -23,6 +26,7 @@ app.get('/oi/:symbol', async (req, res) => {
     const r = await axios.get(url, { headers: HEADERS });
     res.json(r.data);
   } catch(e) {
+    console.error(req.params.symbol, e.message);
     res.status(500).json({ error: e.message });
   }
 });
@@ -39,6 +43,6 @@ app.get('/oichange/:symbol', async (req, res) => {
 });
 
 // Health check
-app.get('/health', (req, res) => res.json({ status: 'ok' }));
+app.get('/health', (req, res) => res.json({ status: 'ok', time: new Date() }));
 
 app.listen(process.env.PORT || 3001, () => console.log('OIZen proxy running'));
